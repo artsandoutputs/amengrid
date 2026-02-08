fbe4aa0a4ad65d97a3d71dab3ec56ad0bfccedee
artsandoutputs <artsandoutputs@gmail.com>
2026-02-08 02:06:42 -0600

    Update page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -11,7 +11,7 @@ import {
   type PatternPack,
   type StepEvent,
   resolveFillOffsets
-} from "./PatternPacks";
+} from "./patternPacks";
 import { LoopPicker } from "./components/LoopPicker";
 import { Waveform } from "./components/Waveform";
 
